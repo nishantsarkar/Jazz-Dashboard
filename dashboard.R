026e2f6768c8aae11fa73c65fdcab8accb0c565c
Nishant <nsrk01@gmail.comm>
2023-07-28 12:52:15 -0400

    yuh

--- dashboard.R
+++ dashboard.R
@@ -3,3 +3,18 @@
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
+
+# Basics
+ui <- dashboardPage(
+  dashboardHeader(title = div(
+    HTML('<img src ="https://www.rylaze.com/media/content/images/og-image.jpg" height = "100px" style = "vertical-align: middle;"/>')
+    )
+  ),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
+
