cef283136874a133d2939b915f7d3566311986b4
Nishant Sarkar <135373493+nishantsarkar@users.noreply.github.com>
2023-07-31 15:21:06 -0400

    Update dashboard.R

--- dashboard.R
+++ dashboard.R
@@ -3,11 +3,12 @@
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
+library(shinyWidgets)
 
 # Basics
 ui <- dashboardPage(
   dashboardHeader(title = div(
-    HTML('<img src ="https://www.rylaze.com/media/content/images/og-image.jpg" height = "100px" style = "vertical-align: middle;"/>')
+    HTML('<img src ="https://www.rylaze.com/media/content/images/og-image.jpg" height = "95px" style = "vertical-align: middle;"/>')
     )
   ),
   # Sidebar menu, remember to label each section with a big title even tho we have the sidebar info
@@ -21,18 +22,67 @@ ui <- dashboardPage(
   )),
   # Individual tabs
   dashboardBody(
+    # Styles that're referenced later (for the text and two image containers)
+    tags$style(HTML("           
+         /* Custom CSS Styles */
+         .pretty-text {
+           font-size: 18px;
+           line-height: 1.4;
+           color: #297e91;
+           text-align: left;
+         }
+         .image-container {
+           display: flex;
+           flex-direction: column;
+           align-items: center;
+         }
+         .dual-image-container {
+           display: flex;
+           justify-content: space-between;
+           align-items: center;
+     }
+    ")),
     tabItems(
-      
+    # First tab content
     tabItem(tabName = "tab1",
-            h2("What is RYLAZE?")
+            h2("What is RYLAZE?", style = "font-weight: bold; font-size: 30px; color: #297e91;"),
+            div(class = "image-container",
+                HTML('<img src="https://www.clinicaltrialsarena.com/wp-content/uploads/sites/22/2021/07/Image-1-Rylaze.jpg" height="450px" style="vertical-align: middle;"/>')
+            ),
+            div(class = "pretty-text",
+                HTML("<p><strong>RYLAZE</strong> (asparaginase erwinia chrysanthemi) is a medication indicated for healthcare providers to treat patients with acute lymphoblastic leukemia (ALL) and hypersensitivity to <i>Escherichia coli</i>-derived asparaginase. As a crucial component of multi-agent chemotherapy regimens, Rylaze works by depleting circulating levels of the amino acid asparagine, thus depriving leukemic cells of this essential nutrient, leading to their growth inhibition and apoptosis.</p>
+                     <p>RYLAZE is the <strong>ONLY</strong> currently-available non-<i>E. coli</i> asparaginase. Furthermore, the innovative recombinant manufacturing process behind RYLAZE is more efficient and reliable than traditional methods of L-asparaginase manufacturing, and the drug comes <strong>ready-to-use.</strong></p>")
+            ),
+            div(class = "dual-image-container",  
+                HTML('<img src="https://www.rylaze.com/media/content/images/dotted-1-x-1000-graphic.svg" style="width: 50%;"/>'),  
+                HTML('<img src="https://www.rylaze.com/media/content/images/dotted-syringe.svg" style="width: 50%;"/>')  
+            ),
+            div(class = "pretty-text",
+                HTML("<p><p><i>This section will include additional first-glance information about Rylaze.</i></p></p>")
+            )
     ),
     
     tabItem(tabName = "tab2",
-            h2("Mechanism of Action")
+            h2("Mechanism of Action", style = "font-weight: bold; font-size: 30px; color: #297e91;"),
+            div(class = "pretty-text",
+                HTML("<p><p>RYLAZE is an <strong>asparaginase</strong> enzyme derived from <i>Erwinia chrysanthemi</i>. The enzyme functions by catalyzing the hydrolysis of asparagine into aspartic acid and ammonia. While healthy cells have the ability to synthesize asparagine, ALL lymphoblasts lack asparagine synthetase, making them reliant on an external supply of asparagine from the bloodstream.</p></p>
+                     <p>Upon administration, Rylaze reduces the circulating levels of asparagine in the body, leading to a condition known as asparagine depletion. As a result, leukemic cells are deprived of this essential nutrient, disrupting their ability to survive and proliferate. The diminished availability of asparagine induces metabolic stress within leukemic cells, leading to cell growth inhibition and eventually triggering apoptosis.</p>
+                     <p>Asparaginase has been an integral component of ALL treatment regiments for over 40 years. The introduction of asparaginase in these protocols has resulted in steady improvements in ALL survival estimates.</p>")
+            ),
+            div(class = "image-container",
+                HTML('<img src="https://www.rylaze.com/media/content/images/charts/chart-improvement-outcomes.svg" height="275px" style="vertical-align: middle;"/>')
+            ),
+            div(class = "pretty-text",
+                HTML("<p>However, the use of <i>E. coli</i>-derived asparaginase may trigger an immune response in some individuals. The antibodies released as a result of this immune response may bind to mast cells and can result in the release of mediators that cause clinical symptoms (including histamine). This can result in hypersensitivity reactions (HSRs). These occur in up to 30% of patients, and the resulting treatment failure can <strong>increase the chance of relapse.</strong></p>
+                     <p>As a non-pathogenic bacterial enzyme derived from Erwinia chrysanthemi, RYLAZE offers a distinct advantage by <strong>bypassing the immune reactions</strong> triggered by <i>E. coli</i>-derived asparaginase. <strong>By switching to RYLAZE</strong>, healthcare providers can continue asparaginase therapy with a lower risk of compromising treatment efficacy or patient safety. This makes it the best-choice non-<i>E. coli</i> asparaginase for ALL.</p>")
+            ),
+            div(class = "image-container",
+                HTML('<img src="https://www.rylaze.com/media/content/images/graphic-immunology.svg" height="160px" style="vertical-align: middle;"/>')
+            ),
     ),
     
     tabItem(tabName = "tab3",
-            h2("Dosage & Administration"),
+            h2("Dosage & Administration", style = "font-weight: bold; font-size: 30px; color: #297e91;"),
             h4("10 mg cristntasparase recombinant in 0.5 mL (20 mg/mL) Solutions for IM Injection"),
             h4("Ready to use, contains six doses to replace previously planned pegaspargase dose"),
             fluidRow(
@@ -48,15 +98,27 @@ ui <- dashboardPage(
     ),
     
     tabItem(tabName = "tab4",
-            h2("Clinical Trial Data")
+            h2("Clinical Trial Data", style = "font-weight: bold; font-size: 30px; color: #297e91;")
     ),
     
     tabItem(tabName = "tab5",
-            h2("Pricing & Insurance")
+            h2("Pricing & Insurance Information", style = "font-weight: bold; font-size: 30px; color: #297e91;"),
+            # Create interactive action buttons for each tab
+            div(class = "pretty-text", 
+                HTML("<p>Click the tabs below to learn about the <strong>pricing and insurance coverage of RYLAZE</strong> in your jurisdiction.</p>")
+            ),
+            div(
+              actionBttn(inputId = "btn_tab1", label = "Ontario", style = "fill", size = "lg"),
+              actionBttn(inputId = "btn_tab2", label = "British Columbia", style = "fill", size = "lg"),
+              actionBttn(inputId = "btn_tab3", label = "Alberta", style = "fill", size = "lg"),
+              actionBttn(inputId = "btn_tab4", label = "Quebec", style = "fill", size = "lg"),
+              htmlOutput("selected_tab_text")
+            # These interact with text in the Server function below
+            )
     ),
     
     tabItem(tabName = "tab6",
-            h2("Additional Information"),
+            h2("Additional Information", style = "font-weight: bold; font-size: 30px; color: #297e91;"),
             h3(style = "text-decoration: underline;","Contact us"),
             h4("Mississauga Corporate Office: 647-946-8801"),
             h4("Medical Information Requests: jazzpharma@medcomsol.com"),
@@ -68,8 +130,48 @@ ui <- dashboardPage(
 )
 
 
+server <- function(input, output) {
+  # Function to update the textbox content based on the selected tab
+  # RenderUI is used so we can include both images and text in this section
+  observeEvent(input$btn_tab1, {
+    output$selected_tab_text <- renderUI({
+      list(
+        HTML('<img src="https://www.ontario.ca/themes/ontario_2021/assets/ontario@2x-print.png" height="150px" style="vertical-align: middle;"/>'),
+        HTML("<div class='pretty-text'><p>Here, information surrounding the pricing and insurance coverage of Rylaze in Ontario can be included.</p></div>")
+      )
+    })
+  })
+  
+  observeEvent(input$btn_tab2, {
+    output$selected_tab_text <- renderUI({
+      list(
+        HTML('<img src="https://images.squarespace-cdn.com/content/v1/5266d49be4b0bc5cd29ee8e0/1484680378995-KGAYS0LQ4HSMSLXEI5TM/image-asset.png" height="150px" style="vertical-align: middle;"/>'),
+        HTML("<div class='pretty-text'><p>Here, information surrounding the pricing and insurance coverage of Rylaze in British Columbia can be included.</p></div>")
+      )
+    })
+  })
+  
+  observeEvent(input$btn_tab3, {
+    output$selected_tab_text <- renderUI({
+      list(
+        HTML('<img src="https://qb-filing.alberta.ca/assets/img/alberta.png" height="150px" style="vertical-align: middle;"/>'),
+        HTML("<div class='pretty-text'><p>Here, information surrounding the pricing and insurance coverage of Rylaze in Alberta can be included.</p></div>")
+      )
+    })
+  })
+  
+  observeEvent(input$btn_tab4, {
+    output$selected_tab_text <- renderUI({
+      list(
+        HTML('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Logo_du_le_gouvernement_du_Qu%C3%A9bec.svg/1530px-Logo_du_le_gouvernement_du_Qu%C3%A9bec.svg.png" height="125px" style="vertical-align: middle;"/>'),
+        HTML("<div class='pretty-text'><p>Here, information surrounding the pricing and insurance coverage of Rylaze in Quebec can be included.</p></div>")
+      )
+    })
+  })
+}
 
-server <- function(input, output) { }
 
 shinyApp(ui, server)
+
+
 
