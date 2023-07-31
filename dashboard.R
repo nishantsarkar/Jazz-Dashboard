826bf5d8cc2ee543781445021e21cf85abecff06
RamizK01 <ramizk2001@gmail.com>
2023-07-31 10:57:49 -0400

    added tabs and headers to each, work under dashboardbody() to add stuff

--- dashboard.R
+++ dashboard.R
@@ -10,9 +10,47 @@ ui <- dashboardPage(
     HTML('<img src ="https://www.rylaze.com/media/content/images/og-image.jpg" height = "100px" style = "vertical-align: middle;"/>')
     )
   ),
-  dashboardSidebar(),
-  dashboardBody()
+  # Sidebar menu, remember to label each section with a big title even tho we have the sidebar info
+  dashboardSidebar(sidebarMenu(
+    menuItem("What is RYLAZE?", tabName = "tab1"),
+    menuItem("Mechanism of Action", tabName = "tab2"),
+    menuItem("Dosage & Administration", tabName = "tab3"),
+    menuItem("Clinical Trial Data", tabName = "tab4"),
+    menuItem("Pricing & Insurance", tabName = "tab5"),
+    menuItem("Additional Information", tabName = "tab6")
+  )),
+  # Individual tabs
+  dashboardBody(
+    tabItems(
+    # First tab content
+    tabItem(tabName = "tab1",
+            h2("What is RYLAZE?")
+    ),
+    
+    tabItem(tabName = "tab2",
+            h2("Mechanism of Action")
+    ),
+    
+    tabItem(tabName = "tab3",
+            h2("Dosage & Administration")
+    ),
+    
+    tabItem(tabName = "tab4",
+            h2("Clinical Trial Data")
+    ),
+    
+    tabItem(tabName = "tab5",
+            h2("Pricing & Insurance")
+    ),
+    
+    tabItem(tabName = "tab6",
+            h2("Additional Information")
+    )
+  )
+  )
 )
+
+
 
 server <- function(input, output) { }
 
